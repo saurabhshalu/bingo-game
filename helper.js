4528d3c64fd4fd14711274c782a2b11485812328
Saurabh Verma <saurabhshalu@gmail.com>
2025-05-25 08:46:20 +0000

    6 digit room number

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 
 import { customAlphabet } from 'nanoid';
 const alphabet = '0123456789';
-export const randomRoomId = customAlphabet(alphabet, 4);
+export const randomRoomId = customAlphabet(alphabet, 6);
 
 export const shuffle = (array) => {
     var m = array.length, t, i;
